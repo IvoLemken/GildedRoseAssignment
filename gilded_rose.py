1b473adbf2d8a1dd362281c75597609bbeca724d
= <=>
2022-09-15 16:40:42 +0200

    Added method descriptions

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,6 +12,10 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
+        """
+            Updates quality and sell_in values for all items
+        """
+
         for item in self.items:
             match item.name :
                 case item.name if re.search("aged brie",item.name,re.IGNORECASE) :
@@ -26,6 +30,11 @@ class GildedRose(object):
                     self.__update_quality_regular(item)
 
     def __update_quality_regular(self, item):
+        """ 
+            Updates regular items:
+            Quality decreases by 1 before, and by 2 after sell by date
+        """
+
         if item.quality > 0:
             if item.sell_in > 0:
                 item.quality -= 1
@@ -35,6 +44,11 @@ class GildedRose(object):
         return item
 
     def __update_quality_aged_brie(self, item):
+        """
+            Updates Aged Brie: 
+            Quality increases by 1 before, and by 2 after sell by date
+        """
+
         if item.quality < MAX_ITEM_QUALITY:
             if item.sell_in > 0:
                 item.quality += 1
@@ -44,6 +58,15 @@ class GildedRose(object):
         return item
 
     def __update_quality_backstage_passes(self, item):
+        """
+            Updates backstage passes:
+            Quality increases:
+                - By 1 when there are more than 10 days until the sell by date
+                - By 2 when there are 10 days or less 
+                - By 3 when there are 5 days or less but
+            Quality drops to 0 after the sell by date
+        """
+
         match item.sell_in: 
             case item.sell_in if item.sell_in > 10 :
                 item.quality = min(item.quality+1,MAX_ITEM_QUALITY)
@@ -57,6 +80,11 @@ class GildedRose(object):
         return item
 
     def __update_quality_conjured(self, item):
+        """
+            Updates conjured items:
+            Quality decreases by 2 before and by 4 after the sell by date
+        """
+        
         if item.quality > 0:
             if item.sell_in > 0:
                 item.quality = max(item.quality-2, 0)
